630fc75e6a67db97aebc7e7203f2bdbc02d55d29
litospayaso <angel.jesuita@gmail.com>
2026-08-18 09:36:29 +0200

    Updates

---
+++ .storybook/main.ts
@@ -0,0 +1,15 @@
+import type { StorybookConfig } from '@storybook/web-components-vite';
+
+const config: StorybookConfig = {
+  "stories": [
+    "../src/**/*.mdx",
+    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-vitest",
+    "@storybook/addon-a11y",
+    "@storybook/addon-docs"
+  ],
+  "framework": "@storybook/web-components-vite"
+};
+export default config;
